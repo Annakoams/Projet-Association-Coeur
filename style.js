b750483d0fa7becf1fb04006428ec83d9bde6277
Annakoams <annanis06@hotmail.com>
2023-01-11 12:07:08 +0100

    Modif inclure javascript

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
 // navbar
 document.addEventListener("DOMContentLoaded", function(){
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
-          document.getElementById('navbarSite').classList.add('fixed-top').css('background:white');
+        if (window.scrollY > 20) {
+          document.getElementById('navbarSite').classList.add('fixed-top');
         } else {
           document.getElementById('navbarSite').classList.remove('fixed-top');
         } 
